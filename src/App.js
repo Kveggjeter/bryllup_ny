1d01231bdc92b3044750753dde8644654417b61e
Kveggjeter <sundbeirik@gmail.com>
2026-02-10 22:29:27 +0100

    masse greier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import oss_1 from './media/oss_1.jpg'
+import redigert from './media/redigert.jpg'
+import hamburger from './media/icons/more.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="frontPage">
+      <div className="out_frontpage">
+        <div className="menu_box">
+          <img className="hamburger" alt="hamburger" src={hamburger}></img>
+          <h2 className="menu_text">Meny</h2>
+        </div>
+          <img className="picture_frontpage" alt="oss" src={redigert} ></img>
+          <div className="bottom_text_box">
+            <h2 className="bottom_text">Vi gleder oss masse til å feire med deg!</h2>
+          </div>
+      </div>
     </div>
+    <div className="formPage">
+    <p>fddddddddddddddddddddddddddddd</p>
+    </div>
+    </>
   );
 }
 
